f254eaac2d461cabd0225bb69a9c3633969c2738
KujithaMannepuri <kujitha120@msitprogram.net>
2019-03-14 10:32:25 +0530

    S04.02-Exercise-DisplayDayForecast

--- module 3/activity 9/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
+++ module 3/activity 9/S04.02-Exercise-DisplayDayForecast/app/src/main/java/com/example/android/sunshine/DetailActivity.java
@@ -1,9 +1,38 @@
+//package com.example.android.sunshine;
+//
+//import android.os.Bundle;
+//import android.support.v7.app.AppCompatActivity;
+//import android.content.Intent;
+//import android.widget.TextView;
+//
+//public class DetailActivity extends AppCompatActivity {
+//
+//    private static final String FORECAST_SHARE_HASHTAG = " #SunshineApp";
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_detail);
+//        TextView weatherTextView = (TextView) findViewById(R.id.tv_display_weather);
+//
+//        // completed (2) Display the weather forecast that was passed from MainActivity
+//        Intent intent = getIntent();
+//
+//        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
+//            String weatherForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
+//            weatherTextView.setText(weatherForecast);
+//        }
+//    }
+//}
+
 package com.example.android.sunshine;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,12 +44,14 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
         TextView weatherTextView = (TextView) findViewById(R.id.tv_display_weather);
 
-        // completed (2) Display the weather forecast that was passed from MainActivity
+        // complete (2) Display the weather forecast that was passed from MainActivity
         Intent intent = getIntent();
 
+//        if (intent != null) {
         if (intent.hasExtra(Intent.EXTRA_TEXT)) {
             String weatherForecast = intent.getStringExtra(Intent.EXTRA_TEXT);
             weatherTextView.setText(weatherForecast);
+//            }
         }
     }
 }
