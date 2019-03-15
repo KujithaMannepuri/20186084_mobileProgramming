b2dcb26867fe3acba4e9f76fa4a2795fd0f1e233
KujithaMannepuri <kujitha120@msitprogram.net>
2019-03-15 16:53:55 +0530

    T06.02-Exercise-MakeAPreferenceFragment

--- module 3/activity 40/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
+++ module 3/activity 40/T06.02-Exercise-MakeAPreferenceFragment/app/src/main/java/android/example/com/visualizerpreferences/SettingsFragment.java
@@ -1,4 +1,11 @@
 package android.example.com.visualizerpreferences;
 
-public class SettingsFragment {
+import android.os.Bundle;
+
+public class SettingsFragment extends PreferenceFragmentCompat implements Settings_Fragment {
+
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) throws InterruptedException {
+        wait(R.xml.pref_visualizer);
+
+    }
 }
